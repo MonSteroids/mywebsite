8cce41d3bc3f60f995a308f4b597fa9fe2bb1765
MonSteroids <112619594+MonSteroids@users.noreply.github.com>
2024-04-29 14:37:45 +0000

    Many changes. Foot added. About and Contact page fixes. Home page added download

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+function downloadImage(imageUrl) {
+    var a = document.createElement('a');
+    a.href = imageUrl;
+    a.download = imageUrl.split('/').pop();
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
